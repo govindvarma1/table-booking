c9b32895421564c7495381b20e7d7776ea299ab4
Kallepalli Sai Govinda Varma <98372421+govindvarma1@users.noreply.github.com>
2025-01-03 19:22:20 +0530

    Fix: @typescript-eslint/no-explicit-any

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -6,25 +6,19 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript",),
-  {
-    rules: {
-      // Turn off warnings for unused variables (useful for development)
-      "@typescript-eslint/no-unused-vars": "off",
-
-      // Allow anonymous default exports (useful for quick prototyping)
-      "import/no-anonymous-default-export": "off",
-
-      // Disable the requirement for React display names in function components
-      "react/display-name": "off",
-    },
-  },
+	...compat.extends("next/core-web-vitals", "next/typescript"),
+	{
+		rules: {
+			"@typescript-eslint/no-unused-vars": "off",
+			"import/no-anonymous-default-export": "off",
+			"react/display-name": "off",
+			"@typescript-eslint/no-explicit-any": "off",
+		},
+	},
 ];
 
 export default eslintConfig;
-
-
